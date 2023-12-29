c37c8626c163cc71814bbef49e7f726e227161fd
elral <3263285+elral@users.noreply.github.com>
2023-12-29 12:57:33 +0100

    copy files per script

---
+++ copy_fw_files.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+mkdir CustomDevices_own/Gagagu/Community/firmware
+cp -r .pio/build/gagagu_fcu_efis_mega/*.hex CustomDevices_own/Gagagu/Community/firmware
+cp -r .pio/build/gagagu_fcu_efis_raspberrypico/*.uf2 CustomDevices_own/Gagagu/Community/firmware
+cd CustomDevices_own/Gagagu/Community
+zip -r -qq gagagu_fcu_efis_${{ github.event.release.tag_name }}.zip *
