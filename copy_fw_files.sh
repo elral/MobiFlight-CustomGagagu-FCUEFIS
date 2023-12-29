02dd524e196523f3c69d89a428b08cd70b41a144
elral <3263285+elral@users.noreply.github.com>
2023-12-29 13:11:50 +0100

    release version in file name

--- copy_fw_files.sh
+++ copy_fw_files.sh
@@ -4,4 +4,4 @@ mkdir CustomDevices_own/Gagagu/Community/firmware
 cp -r .pio/build/gagagu_fcu_efis_mega/*.hex CustomDevices_own/Gagagu/Community/firmware
 cp -r .pio/build/gagagu_fcu_efis_raspberrypico/*.uf2 CustomDevices_own/Gagagu/Community/firmware
 cd CustomDevices_own/Gagagu/Community
-zip -r -qq gagagu_fcu_efis_0.1.0.zip *
+zip -r -qq gagagu_fcu_efis_${ github.event.release.tag_name }.zip *
