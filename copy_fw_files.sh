db0109c998743d4ece8828011c097735f1e735aa
elral <3263285+elral@users.noreply.github.com>
2023-12-30 08:52:21 +0100

    using absolute path

--- copy_fw_files.sh
+++ copy_fw_files.sh
@@ -3,8 +3,8 @@
 custom_device_path='CustomDevices_own/Gagau'
 zip_file_name='gagagu_fcu_efis'
 
-mkdir $custom_device_path/Community/firmware
-cp -r .pio/build/**/*.hex $custom_device_path/Community/firmware
-cp -r .pio/build/**/*.uf2 $custom_device_path/Community/firmware
-cd $custom_device_path/Community
+mkdir ./$custom_device_path/Community/firmware
+cp -r .pio/build/**/*.hex ./$custom_device_path/Community/firmware
+cp -r .pio/build/**/*.uf2 ./$custom_device_path/Community/firmware
+cd ./$custom_device_path/Community
 zip -r -qq $zip_file_name_$VERSION.zip *
