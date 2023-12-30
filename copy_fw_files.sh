c3cd27ecc5347d346415298bda85bbe51b7c2435
elral <3263285+elral@users.noreply.github.com>
2023-12-30 10:25:14 +0100

    correct path for zip file

--- copy_fw_files.sh
+++ copy_fw_files.sh
@@ -6,5 +6,5 @@ zip_file_name='gagagu_fcu_efis'
 mkdir $custom_device_path/Community/firmware
 cp -r .pio/build/**/*.hex $custom_device_path/Community/firmware
 cp -r .pio/build/**/*.uf2 $custom_device_path/Community/firmware
-cd /CustomDevices_own/Gagau/Community
+cd $custom_device_path/Community
 zip -r -qq $zip_file_name_$VERSION.zip *
