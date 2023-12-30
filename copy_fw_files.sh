bc2a3262ccaddd2868bdddf7e7c82a9d4ce292f3
elral <3263285+elral@users.noreply.github.com>
2023-12-30 13:55:09 +0100

    fix filename in zip file

--- copy_fw_files.sh
+++ copy_fw_files.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 custom_device_path='CustomDevices_own/Gagagu'
-zip_file_name=gagagu_fcu_efis
+zip_file_name='gagagu_fcu_efis'
 
 mkdir $custom_device_path/Community/firmware
 cp -r .pio/build/**/*.hex $custom_device_path/Community/firmware
 cp -r .pio/build/**/*.uf2 $custom_device_path/Community/firmware
-cd $custom_device_path/Community
-zip -r -qq $zip_file_name_$VERSION.zip *
+cd "$custom_device_path"/Community
+zip -r -qq "$zip_file_name"_"$VERSION".zip *
