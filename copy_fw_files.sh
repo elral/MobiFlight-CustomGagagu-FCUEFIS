f2a7d234aecaab82292eb9a62905efb153e4a0b4
elral <3263285+elral@users.noreply.github.com>
2023-12-30 10:22:19 +0100

    correct path

--- copy_fw_files.sh
+++ copy_fw_files.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-custom_device_path='CustomDevices_own/Gagau'
+custom_device_path='CustomDevices_own/Gagagu'
 zip_file_name='gagagu_fcu_efis'
 
 mkdir $custom_device_path/Community/firmware
