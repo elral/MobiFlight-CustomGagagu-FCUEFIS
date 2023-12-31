6a24a036894f0af7e1b175462750385eba2f2f6c
elral <3263285+elral@users.noreply.github.com>
2023-12-31 09:58:11 +0100

    more clean up

--- copy_fw_files.py
+++ copy_fw_files.py
@@ -2,7 +2,9 @@ Import("env")
 import os
 import shutil
 
-dirname = "CustomDevices_own/Gagagu/Community"
+custom_device_path = "CustomDevices_own/Gagagu/Community"
+
+
 # Get the version number from the build environment.
 firmware_version = os.environ.get('VERSION', "")
 
@@ -10,13 +12,13 @@ def copy_fw_files (source, target, env):
     fw_file_name=str(target[0])
     print("Endung ist: " + fw_file_name[-3:])
 
-    if os.path.exists(dirname + "/firmware") == False:
-        os.mkdir(dirname + "/firmware")
+    if os.path.exists(custom_device_path + "/firmware") == False:
+        os.mkdir(custom_device_path + "/firmware")
     
     if fw_file_name[-3:] == "bin":
         fw_file_name=fw_file_name[0:-3] + "uf2"
 
-    shutil.copy(fw_file_name, dirname + "/firmware")
+    shutil.copy(fw_file_name, custom_device_path + "/firmware")
     pass
 
 
--- create_zip_file.sh
+++ create_zip_file.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-custom_device_path='CustomDevices_own/Gagagu'
+custom_device_path='CustomDevices_own/Gagagu/Community'
 zip_file_name='gagagu_fcu_efis'
 
-cd "$custom_device_path"/Community
+
+
+cd "$custom_device_path"
 zip -r -qq "$zip_file_name"_"$VERSION".zip *
