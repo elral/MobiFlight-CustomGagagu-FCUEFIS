7ba0e596ddd0ceebc91a1c0315b0a2e010551239
elral <3263285+elral@users.noreply.github.com>
2023-12-31 09:46:28 +0100

    fixed copy uf2 files

--- copy_fw_files.py
+++ copy_fw_files.py
@@ -8,16 +8,23 @@ dirname = "CustomDevices_own/Gagagu/Community"
 firmware_version = os.environ.get('VERSION', "")
 
 def copy_fw_files (source, target, env):
+    fw_file_name=str(target[0])
+    print("Endung ist: " + fw_file_name[-3:])
+
     if os.path.exists(dirname + "/firmware") == False:
         os.mkdir(dirname + "/firmware")
     
-    shutil.copy(str(target[0]), dirname + "/firmware")
+    if fw_file_name[-3:] == "bin":
+        fw_file_name=fw_file_name[0:-3] + "uf2"
+
+    shutil.copy(fw_file_name, dirname + "/firmware")
     pass
 
 #def create_zip_folder (source, target, env):
 #    os.chdir(dirname)
 #    subprocess.run(["zip", "-r -qq gagau_fcu_efis_" + firmware_version + ".zip *"])
 #    pass
+
 
 env.AddPostAction("$BUILD_DIR/${PROGNAME}.hex", copy_fw_files)
 env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", copy_fw_files)
