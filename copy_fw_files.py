b08df11b02458413fa70cd8fc21137034d5b6666
elral <3263285+elral@users.noreply.github.com>
2024-01-02 07:54:46 +0100

    create zip folder from python script

--- copy_fw_files.py
+++ copy_fw_files.py
@@ -1,26 +1,41 @@
 Import("env")
-import os
-import shutil
+import os, zipfile, shutil
 
-custom_device_path = "CustomDevices_own/Gagagu/Community"
 
+custom_device_path = "CustomDevices_own/Gagagu"
+zip_file_name = "gagagu_fcu_efis"
 
 # Get the version number from the build environment.
 firmware_version = os.environ.get('VERSION', "")
+if firmware_version == "":
+  firmware_version = "0.0.1"
+firmware_version = firmware_version.lstrip("v")
+firmware_version = firmware_version.strip(".")
+
 
 def copy_fw_files (source, target, env):
-    fw_file_name=str(target[0])
-    print("Endung ist: " + fw_file_name[-3:])
+#    if firmware_version == "0.0.1":
+#        return
 
-    if os.path.exists(custom_device_path + "/firmware") == False:
-        os.mkdir(custom_device_path + "/firmware")
+    fw_file_name=str(target[0])
+
+    if os.path.exists(custom_device_path + "/Community/firmware") == False:
+        os.mkdir(custom_device_path + "/Community/firmware")
     
     if fw_file_name[-3:] == "bin":
         fw_file_name=fw_file_name[0:-3] + "uf2"
 
-    shutil.copy(fw_file_name, custom_device_path + "/firmware")
-    pass
+    shutil.copy(fw_file_name, custom_device_path + "/Community/firmware")
+    createZIP(fw_file_name)
 
+
+def createZIP(filename,compress = zipfile.ZIP_DEFLATED):
+    complete_ZIP_Filename = custom_device_path + '/' + zip_file_name + '_' + firmware_version + '.zip'
+    with zipfile.ZipFile(complete_ZIP_Filename, 'w', compress) as target:
+        for root, dirs, files in os.walk(custom_device_path + "/Community"):
+            for file in files:
+                add = os.path.join(root, file)
+                target.write(add)
 
 
 env.AddPostAction("$BUILD_DIR/${PROGNAME}.hex", copy_fw_files)
